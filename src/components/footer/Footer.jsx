55fc8b4a5cfcbe8eda4ae90caa71310f421f3dd4
SWASTI SUNDAR PRADHAN <swastipradhan044@gmail.com>
2023-08-03 21:57:58 +0530

    testing2

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,9 +6,8 @@ import {
     FaLinkedin,
 } from "react-icons/fa";
 
-import ContentWrapper from "../contentWrapper/ContentWrapper";
-
 import "./style.scss";
+import ContentWrapper from "../contentWrapper/ContentWrapper";
 
 const Footer = () => {
     return (
